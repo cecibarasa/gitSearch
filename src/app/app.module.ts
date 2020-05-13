f4951d9b9c0abf75ee044cf1e3adb26485c086af
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-13 17:19:13 +0300

    add repo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitsearchComponent } from './gitsearch/gitsearch.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitsearchComponent
+    GitsearchComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
