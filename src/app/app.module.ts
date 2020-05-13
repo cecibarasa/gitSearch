9e73f036dad69e6bb7d3a0a4d6b9b09ebb2c7b03
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-13 17:52:03 +0300

    add search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GitsearchComponent } from './gitsearch/gitsearch.component';
 import { RepoComponent } from './repo/repo.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitsearchComponent,
     RepoComponent,
-    NavBarComponent
+    NavBarComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
