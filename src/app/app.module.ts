ba0fcac1f2d23146738443aad4ec866a89af9af0
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-13 17:50:02 +0300

    add nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitsearchComponent } from './gitsearch/gitsearch.component';
 import { RepoComponent } from './repo/repo.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitsearchComponent,
-    RepoComponent
+    RepoComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
