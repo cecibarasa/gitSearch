77c132cfb329860e33c3ba83a8aa0b7b16c2b15e
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-16 07:55:31 +0300

    update not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GitsearchComponent } from "./gitsearch/gitsearch.component";
 import { RepositoriesComponent } from "./repositories/repositories.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 
 const routes: Routes = [
   { path: 'gitSearch', component: GitsearchComponent},
   { path: 'repositories', component: RepositoriesComponent},
   { path: '', redirectTo:"/gitSeach", pathMatch:"full"},
-  //{ path:'**', component:NotFoundComponent},
+  { path:'**', component:NotFoundComponent},
 
 ];
 
