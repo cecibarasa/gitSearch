3ae88d6de7ae02c179ed02ef09ac8fc4a07f18e3
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-16 17:31:32 +0300

    add changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GitsearchComponent } from "./gitsearch/gitsearch.component";
 import { RepositoriesComponent } from "./repositories/repositories.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 
 const routes: Routes = [
+  { path: 'landingpage', component: LandingPageComponent},
   { path: 'gitSearch', component: GitsearchComponent},
   { path: 'repositories', component: RepositoriesComponent},
-  { path: '', redirectTo:"/gitSeach", pathMatch:"full"},
+  { path: '', redirectTo:"/landingpage", pathMatch:"full"},
   { path:'**', component:NotFoundComponent},
 
 ];
--- src/app/change-color.directive.ts
+++ src/app/change-color.directive.ts
@@ -6,10 +6,9 @@ import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 export class ChangeColorDirective {
 
   constructor(private element: ElementRef, private renderer: Renderer2) { 
-    this.changeColor('red');
+    this.changeColor;
   }
-  @HostListener('click') foo(){
-    alert('Host Element is clicked');
+  @HostListener('click') onClick(){
     this.changeColor('green');
 }
 changeColor(color: string){
